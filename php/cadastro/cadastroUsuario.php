7e8b309c191d99f0f81a09183a839a204012c2c1
Eduardo Siegert Antunes <eantunes5@hotmail.com>
2021-05-15 18:50:22 -0300

    Att bancoEc

--- php/cadastro/cadastroUsuario.php
+++ php/cadastro/cadastroUsuario.php
@@ -3,12 +3,17 @@
 include_once("../config/conexao.php");
 
 // recebe dados do formulário
-$nome = filter_input(INPUT_POST, 'campoNome', FILTER_SANITIZE_STRING);
-$nascimento = filter_input(INPUT_POST, 'campoNasc');
-$email = filter_input(INPUT_POST, 'campoEmail', FILTER_SANITIZE_EMAIL);
-$senha = filter_input(INPUT_POST, 'senha_hash');
+$nome = filter_input(INPUT_POST, 'nome_completo', FILTER_SANITIZE_STRING);
+$nascimento = filter_input(INPUT_POST, 'data_nascimento');
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$senha = filter_input(INPUT_POST, 'senha');
+$numero_cartao = filter_input(INPUT_POST, 'numero_cartao', FILTER_SANITIZE_STRING);
+$validade_cartao = filter_input(INPUT_POST, 'validade_cartao', FILTER_SANITIZE_NUMBER_INT);
+$cvv = filter_input(INPUT_POST, 'cvv');
+$titular = filter_input(INPUT_POST, 'titular', FILTER_SANITIZE_NUMBER_INT);
+$cpf_cnpj = filter_input(INPUT_POST, 'cpf_cnpj', FILTER_SANITIZE_NUMBER_INT);
 
-$result_usuario = "INSERT INTO usuarios (campoNome, campoNasc, email, senha) VALUE ('$nome', '$nascimento', '$email', md5('$senha'))";
+$result_usuario = "INSERT INTO usuarios (nome_completo, data_nascimento, email, senha, numero_cartao, validade_cartao, cvv, titular, cpf_cnpj) VALUES ('$nome', '$nascimento', '$email', md5('$senha'), '$numero_cartao', '$validade_cartao', '$cvv', '$titular', '$cpf_cnpj')";
 
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
