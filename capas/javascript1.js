edc119f9ac20b9678c9c61a11054e80e5830a110
UwUsca <63320956+UwUsca@users.noreply.github.com>
2021-06-16 19:05:21 -0300

    Delete capas directory

--- capas/javascript1.js
+++
@@ -1,8 +0,0 @@
-const card = document.querySelector('.card_inner');
-
-card.addEventListener('mouseenter',function(){
-    card.classList.toggle('is-flipped');
-})
-card.addEventListener('mouseleave',function(){
-    card.classList.toggle('is-flipped');
-})
